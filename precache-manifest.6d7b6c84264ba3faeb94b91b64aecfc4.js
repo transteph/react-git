77fd082e648269e9099a1fb45c4d444f78ef8cb4
transteph <transteph@protonmail.com>
2019-05-13 20:00:12 -0400

    Updates

---
+++ precache-manifest.6d7b6c84264ba3faeb94b91b64aecfc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd3cf6d3c6c952b4ea8231227cf7af5",
+    "url": "/react-git/index.html"
+  },
+  {
+    "revision": "5740d98894c87e3525ee",
+    "url": "/react-git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5740d98894c87e3525ee",
+    "url": "/react-git/static/js/main.f47797d9.chunk.js"
+  },
+  {
+    "revision": "18d0e6a5410d25ccea4b",
+    "url": "/react-git/static/js/runtime~main.8581ea9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-git/static/media/logo.5d5d9eef.svg"
+  }
+]);
